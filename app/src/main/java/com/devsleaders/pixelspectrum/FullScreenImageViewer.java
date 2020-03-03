eb8045273f888cc567a963dbf09a583d3edb5862
Soniakona <soniahossainkona@gmail.com>
2020-03-03 08:05:01 +0600

    fixed some bugs

--- app/src/main/java/com/devsleaders/pixelspectrum/FullScreenImageViewer.java
+++ app/src/main/java/com/devsleaders/pixelspectrum/FullScreenImageViewer.java
@@ -3,12 +3,25 @@ package com.devsleaders.pixelspectrum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FullScreenImageViewer extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image_viewer);
+
+        if(getIntent().hasExtra("position") && getIntent().hasExtra("images")){
+
+            int images[] = getIntent().getIntArrayExtra("images");
+            int position = getIntent().getIntExtra("position", 0);
+
+            imageView = findViewById(R.id.fullscreenImageId);
+
+            imageView.setBackgroundResource(images[position]);
+        }
     }
 }
